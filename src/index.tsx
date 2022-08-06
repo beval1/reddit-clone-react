23b73fbea60a9a7e0e9a29f4596562b7022af800
beval1 <bobo91280@gmail.com>
2022-08-06 19:20:47 +0300

    Added dependencies: materialui, react router, material-icons. Added a few pages which are not functional yet Better Folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/log-in/LoginPage";
+import RegisterPage from "./pages/register/RegisterPage";
+import NotFoundPage from "./pages/not-found/NotFoundPage";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/log-in" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Header = (props: Props) => {
+  return (
+    <header>
+      <nav></nav>
+    </header>
+  );
+};
+
+export default Header;
